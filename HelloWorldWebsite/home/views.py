903c2496e2826e9bc0263c355aa8548a5c1c7930
ldmcdona <ldmcdona@ualberta.ca>
2019-09-07 16:17:06 -0600

    added idea for sql search

--- HelloWorldWebsite/home/views.py
+++ HelloWorldWebsite/home/views.py
@@ -6,6 +6,7 @@ from django.template import loader
 from django.http import HttpResponse
 from django.views import generic
 from .models import Counter
+from django.db import models
 
 # Create your views here.
 class Home(generic.DetailView):
@@ -21,3 +22,35 @@ class Home(generic.DetailView):
         counter_object.count += 1
         counter_object.save()
         return redirect('homepage')
+
+class Finder(generic.DetailView):
+    longitude = models.IntegerField()
+    latitude = models.IntegerField()
+    neighbourhood = models.CharField(max_length=100)
+
+    def nearest(self, request, *args, **kwargs):
+        return -1
+
+'''
+import mysql.connector
+
+location = pos
+long = pos.x
+lati = pos.y
+
+mydb = mysql.connector.connect(
+    host = "",
+    user = "",
+    passwd = "",
+    database = ""
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("
+SELECT *
+FROM tree
+ORDER BY sqrt( (lat - orig_lat)**2 + (long - orig_long)**2 ) DESC
+LIMIT 1;
+")
+'''
